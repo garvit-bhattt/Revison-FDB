318c11886974b7143f1d53d7b4474697c7b8a2f0
GARVIT <garvitbhattpc@gmail.com>
2025-12-18 14:34:00 +0530

    ques 5  added

---
+++ ques-5/script.js
@@ -0,0 +1,11 @@
+// Step 1: Get required DOM elements
+var text = document.getElementById("text");
+var styleBtn = document.getElementById("styleBtn");
+
+// Step 2: Add click event
+styleBtn.addEventListener("click", function () {
+
+  // Write your logic here
+  // Add or remove "highlight" class
+
+});

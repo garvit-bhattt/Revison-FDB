47afd4a38b44329879b6ff08c6f4c5e174afbce7
GARVIT <garvitbhattpc@gmail.com>
2025-12-18 14:36:59 +0530

    ques 6 added

---
+++ ques-6/script.js
@@ -0,0 +1,10 @@
+// Step 1: Get required DOM elements
+var toggleBtn = document.getElementById("toggleBtn");
+var profileImg = document.getElementById("profileImg");
+
+// Step 2: Add click event listener
+toggleBtn.addEventListener("click", function () {
+
+  // Write your logic here
+
+});

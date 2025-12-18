669a8fe22404ac5a73ee6c8be225d0dd455cbdc3
GARVIT <garvitbhattpc@gmail.com>
2025-12-18 12:16:36 +0530

    ques 4 added

---
+++ ques-4/script.js
@@ -0,0 +1,11 @@
+// Step 1: Get required DOM elements
+var highlightBtn = document.getElementById("highlightBtn");
+var box = document.getElementById("box");
+
+// Step 2: Add click event
+highlightBtn.addEventListener("click", function () {
+
+  // Write your logic here
+  // Add or remove "highlight" class on box
+
+});

78a2d029ea05ae0cf2c3035fc94e8572ca34cfdf
GARVIT <garvitbhattpc@gmail.com>
2025-12-18 11:56:04 +0530

    ques added

---
+++ ques-2/script.js
@@ -0,0 +1,15 @@
+// Step 1: Get required DOM elements
+var taskInput = document.getElementById("taskInput");
+var addBtn = document.getElementById("addBtn");
+var taskList = document.getElementById("taskList");
+
+// Step 2: Add event listener to button
+addBtn.addEventListener("click", function () {
+
+  // Step 3: Write logic to add task
+  // (same as previous question)
+
+});
+
+// Step 4: Add click event to delete a task
+// Write logic here

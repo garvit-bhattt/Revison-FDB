2ab416f635e9a73a99884748a6f82fe0ff45db3f
GARVIT <garvitbhattpc@gmail.com>
2025-12-18 11:37:59 +0530

    first ques js

--- ques-1/script.js
+++ ques-1/script.js
@@ -1,1 +1,19 @@
-// Write your solution here
+// Step 1: Get required DOM elements
+var taskInput = document.getElementById("taskInput");
+var addBtn = document.getElementById("addBtn");
+var taskList = document.getElementById("taskList");
+
+// Step 2: Add event listener to button
+addBtn.addEventListener("click", function () {
+
+  // Step 3: Read input value
+  var taskValue = taskInput.value;
+
+  // Step 4: Write your logic here
+  // - check empty input
+  // - create <li>
+  // - set text
+  // - append to list
+  // - clear input
+
+});

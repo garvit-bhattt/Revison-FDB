949770a09e908a412d097ba6f79b74694d7f9c48
GARVIT <garvitbhattpc@gmail.com>
2025-12-18 14:48:47 +0530

    ques7  added

---
+++ ques-7/script.js
@@ -0,0 +1,10 @@
+// Step 1: Get required DOM elements
+var nameInput = document.getElementById("nameInput");
+var submitBtn = document.getElementById("submitBtn");
+
+// Step 2: Add event listener on input
+nameInput.addEventListener("input", function () {
+
+  // Write your logic here
+
+});
